2f3933fdacd00c3d2d0379f5215dd5073a501cdb
Nathan Bell <nathanbell09@gmail.com>
2018-10-15 13:03:45 +0000

    automatically backed up by learn

--- search_enumerables.rb
+++ search_enumerables.rb
@@ -6,5 +6,5 @@ num_array.select{ |num| num.odd? } #=> [1, 3, 5]
 num_array.find{ |num| num.even? } #=> 2
 num_array.find{ |num| num.odd? } #=> 1
 
-puts num_array.reject{ |num| num.even? } #=> [1, 3, 5]
-puts num_array.reject{ |num| num.odd? } #=> [2, 4, 6]
+num_array.reject{ |num| num.even? } #=> [1, 3, 5]
+num_array.reject{ |num| num.odd? } #=> [2, 4, 6]
